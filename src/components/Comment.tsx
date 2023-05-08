08d8e199f0ad5016e1fd81fa917937621fbff8bd
Matheus Santos <mathehensan@gmail.com>
2023-05-08 00:06:27 -0300

    Initial commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,61 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+import { Avatar } from './Avatar'
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (content: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar
+                hasBorder={false}
+                src="https://avatars.githubusercontent.com/u/48302894?v=4"
+                alt=""
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Matheus Santos</strong>
+                            <time title="11 de Maio às 8:13" dateTime="2022-05-11 08:13:45">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button
+                            onClick={handleDeleteComment}
+                            title="Deletar comentário"
+                        >
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Post.tsx
@@ -0,0 +1,136 @@
+import styles from './Post.module.css'
+
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import { Comment } from './Comment'
+import { Avatar } from './Avatar'
+import { useState, FormEvent, ChangeEvent, InvalidEvent } from 'react'
+
+interface Author {
+    name: string;
+    avatarURL: string;
+    role: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+export interface PostType {
+    id: number;
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+interface PostProps {
+    post: PostType;
+}
+
+export function Post({ post }:PostProps) {
+
+    const [comments, setComments] = useState([
+        "Post muito bacana, hein!",
+    ])
+
+    const [newCommentText, setNewCommentText] = useState('');
+
+    const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
+    function handleCreateComment(event: FormEvent) {
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value);
+    }
+
+    function handleNewInvalidComment(event: InvalidEvent<HTMLTextAreaElement>) {
+        event.target.setCustomValidity('Não é permitido comentários vazios');
+    }
+
+    function deleteComment(commentToDelete: string) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        });
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+
+    return (
+
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={post.author.avatarURL}/>
+                    <div className={styles.authorInfo}>
+                        <strong>{post.author.name}</strong>
+                        <span>{post.author.role}</span>
+                    </div>
+                </div>
+                <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
+            </header>
+
+            <div className={styles.content}>
+                {post.content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>
+                    }
+                })}
+                <p><a href="#">#novoprojeto</a>{' '}<a href="#">#nlw</a>{' '}<a href="#">#rocketseat</a></p>
+            </div>
+
+            <form onSubmit={handleCreateComment} className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    name="comment"
+                    placeholder="Deixe um comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewInvalidComment}
+                    required
+                />
+                <footer>
+                    <button
+                        type="submit"
+                        disabled={isNewCommentEmpty}
+                    >
+                        Publicar
+                    </button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                {comments.map(comment => {
+                    return (
+                        <Comment
+                            key={comment}
+                            content={comment}
+                            onDeleteComment={deleteComment}
+                        />
+                    )
+                })}
+            </div>
+        </article>
+    )
+}
